c22251b68bf95852df53733a205f62089ea36cb7
Drew Wilken <wilk0001@stthomas.edu>
2018-05-15 18:19:26 -0500

    Added files

---
+++ ConwayLife/GraphViewController.swift
@@ -0,0 +1,37 @@
+//
+//  GraphViewController.swift
+//  ConwayLife
+//
+//  Created by Drew Wilken on 11/8/17.
+//  Copyright © 2017 Drew Wilken. All rights reserved.
+//
+
+import UIKit
+
+class GraphViewController: UIViewController {
+    @IBOutlet weak var graphView: UIView!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //graphView
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
